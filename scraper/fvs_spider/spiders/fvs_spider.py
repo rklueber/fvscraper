54e6c6968627d3156f0bc2db4a7c1d7209d695f5
Ralf Klüber <r@lf-klueber.de>
2022-02-02 13:58:15 +0100

    ...

--- scraper/fvs_spider/spiders/fvs_spider.py
+++ scraper/fvs_spider/spiders/fvs_spider.py
@@ -14,9 +14,17 @@ class FvSSpider(Spider):
         fondsdata = response.css('div[data-id]')[0].css('tr')
         fondsprices = response.css('div[data-id]')[1].css('tr')
         for i in range(1,len(fondsdata)):
+            
+            isin =     fondsdata[i  ].xpath('td//text()')[1].get())
+            price =    fondsprices[i].xpath('td//text()')[1].get().split(' ')[0])
+            currency = fondsprices[i].xpath('td//text()')[1].get().split(' ')[1])
+            datafrom = datetime.strptime(fondsprices[i].xpath('td//text()')[4].get(), "%d.%m.%Y").strftime("%Y-%m-%d"))
+            
+            print("=== ", isin, price, currency, datafrom)
+            
             item = ItemLoader(FvSSpiderItem())
-            item.add_value('isin',     fondsdata[i  ].xpath('td//text()')[1].get())
-            item.add_value('price',    fondsprices[i].xpath('td//text()')[1].get().split(' ')[0])
-            item.add_value('currency', fondsprices[i].xpath('td//text()')[1].get().split(' ')[1])
-            item.add_value('datafrom', datetime.strptime(fondsprices[i].xpath('td//text()')[4].get(), "%d.%m.%Y").strftime("%Y-%m-%d"))
+            item.add_value('isin',     isin)
+            item.add_value('price',    price)
+            item.add_value('currency', currency)
+            item.add_value('datafrom', datafrom)
             yield item.load_item()
