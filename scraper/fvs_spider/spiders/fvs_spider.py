62721ee5a61021a5d55d33d5ba138fa7f05402a1
Ralf Klüber <r@lf-klueber.de>
2022-02-02 14:40:44 +0100

    ...

--- scraper/fvs_spider/spiders/fvs_spider.py
+++ scraper/fvs_spider/spiders/fvs_spider.py
@@ -13,15 +13,15 @@ class FvSSpider(Spider):
     def parse(self, response):
         fondsdata = response.css('div[data-id]')[0].css('tr')
         fondsprices = response.css('div[data-id]')[1].css('tr')
-        for i in range(2,len(fondsdata)):
+        for i in range(1,len(fondsdata)):
+            print("############ ",i)
             if len(fondsprices[i].xpath('td//text()')) > 4:
                 isin =     fondsdata[i  ].xpath('td//text()')[1].get()
                 price =    fondsprices[i].xpath('td//text()')[1].get()
                 datafrom = fondsprices[i].xpath('td//text()')[4].get()
                 print("=== ", i, isin, price, datafrom)
-
-                price    = price.split(' ')[0] 
                 currency = price.split(' ')[1]
+                price    = price.split(' ')[0] 
                 item = ItemLoader(FvSSpiderItem())
                 item.add_value('isin',     isin)
                 item.add_value('price',    price)
